f027a56bc3ae615be163393559c2e26693064f02
vigneshksaithal <60360863+vigneshksaithal@users.noreply.github.com>
2020-12-10 22:38:02 +0530

    Delete prod.js

--- public/firebase/prod.js
+++
@@ -1,2 +0,0 @@
-
-var firebaseConfig={apiKey:"AIzaSyB268KjjM7SMtgeZG7Zq4Gl1ozPrMFef_g",authDomain:"vipatra-3d522.firebaseapp.com",databaseURL:"https://vipatra-3d522.firebaseio.com",projectId:"vipatra-3d522",storageBucket:"vipatra-3d522.appspot.com",messagingSenderId:"146612162751",appId:"1:146612162751:web:6dd64e26e26d535861b60a",measurementId:"G-R3DTVJTV26"};firebase.initializeApp(firebaseConfig);
