9f52cbfd5f2515be45744c0998d1194a964ec0f3
vigneshksaithal <60360863+vigneshksaithal@users.noreply.github.com>
2020-12-12 16:59:32 +0530

    Create json-to-csv-download.js

---
+++ public/plugins/json-to-csv-download.js
@@ -0,0 +1,14 @@
+function json_to_csv_download(data, fileName) {     
+  const replacer = (key, value) => value === null ? '' : value       
+  /* specify how you want to handle null values */      
+  const header = Object.keys(data[0])
+  let csv = items.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','))
+csv.unshift(header.join(','))
+csv = csv.join('\r\n')
+
+  var hiddenElement = document.createElement('a')
+  hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv)
+  hiddenElement.target = '_blank'
+  hiddenElement.download = fileName
+  hiddenElement.click()
+} 
