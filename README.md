046b2428aec4302593d267beef6233c48c9df8f7
vigneshksaithal <vigneshksaithal@users.noreply.github.com>
2020-12-13 09:04:29 +0000

    Prettified Code!

--- README.md
+++ README.md
@@ -2,12 +2,13 @@ Vipatra is a billing software for small business
 
 For contributing please clearly explain the issue and it will be accepted if it solves the problem.
 
-
 #languages
+
 1. HTML
 2. CSS
 3. Javascript
 
 #frameworks
+
 1. Vuejs
 2. Bootstrap
--- public/plugins/json-to-csv-download.js
+++ public/plugins/json-to-csv-download.js
@@ -1,14 +1,18 @@
-function json_to_csv_download(data, fileName) {     
-  const replacer = (key, value) => value === null ? '' : value       
-  /* specify how you want to handle null values */      
-  const header = Object.keys(data[0])
-  let csv = data.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','))
-  csv.unshift(header.join(','))
-  csv = csv.join('\r\n')
+function json_to_csv_download(data, fileName) {
+  const replacer = (key, value) => (value === null ? "" : value);
+  /* specify how you want to handle null values */
+  const header = Object.keys(data[0]);
+  let csv = data.map((row) =>
+    header
+      .map((fieldName) => JSON.stringify(row[fieldName], replacer))
+      .join(",")
+  );
+  csv.unshift(header.join(","));
+  csv = csv.join("\r\n");
 
-  var hiddenElement = document.createElement('a')
-  hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv)
-  hiddenElement.target = '_blank'
-  hiddenElement.download = fileName + '.csv'
-  hiddenElement.click()
-} 
+  var hiddenElement = document.createElement("a");
+  hiddenElement.href = "data:text/csv;charset=utf-8," + encodeURI(csv);
+  hiddenElement.target = "_blank";
+  hiddenElement.download = fileName + ".csv";
+  hiddenElement.click();
+}
